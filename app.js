d11fce5206478190413425c525caac855f5badac
hortune <b04611015@ntu.edu.tw>
2016-05-21 02:50:51 -0700

    adding easy register html and implement post

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 var express = require('express')
+var bodyParser = require('body-parser');
 var app = express()
-
+var mail = require ('./mail_work/mail');
+var encrypt = require ('./mail_work/encrypt');
 app.set('view engine', 'ejs');  
-
+app.use(bodyParser.urlencoded({extended: true })); 
 var data = { body: 'map' }
 app.get('/', function(req, res) { res.render('layout', data) })
-
-
+app.get('/register',function(req,res){res.sendFile('/mail_work/index.html', { root: __dirname });})
+app.post('/myaction', function(req, res) {
+  res.send('You sent the name "' + req.body.studentID+ '".');
+  mail.sendEmail('ntuosc@gmail.com','12323','123213');
+});
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 })
+
+
+
+
+
+
--- mail_work/node.js
+++ mail_work/node.js
@@ -1,6 +1,7 @@
-var mail = require ('./mail');
-mail.sendEmail('ntuosc@gmail.com','12323','123213');
-var encrypt = require ('./encrypt');
+
 // equivalent of csv.createCsvFileReader('data.csv') 
 
 
+var mail = require ('./mail');
+mail.sendEmail('ntuosc@gmail.com','12323','123213');
+var encrypt = require ('./encrypt');
